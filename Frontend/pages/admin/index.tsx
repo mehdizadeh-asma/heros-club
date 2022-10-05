017ea615c9b1e06b848e755f64bb89a7aac86dd3
Asma Mehdizadeh <mehdizadeh.asma@gmail.com>
2022-10-06 00:34:12 +0330

    Remove CSS File Admin Page

--- Frontend/pages/admin/index.tsx
+++ Frontend/pages/admin/index.tsx
@@ -2,8 +2,6 @@ import Head from "next/head";
 
 import DashboardLayout from "@/components/UI/Dashbords/DashboardLayout";
 
-import styles from "@/pages/admin/index.module.css";
-
 export default function Dashboard() {
   return (
     <div>
@@ -11,12 +9,9 @@ export default function Dashboard() {
         <title>Heros Club Administrator Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <DashboardLayout />
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
