4dddfdac77f5b1266d4c92f5a01ffd72b622190f
Asma Mehdizadeh <mehdizadeh.asma@gmail.com>
2022-10-06 00:28:40 +0330

    Add Validation To FloatingInput Component

--- Frontend/components/UI/Form/FloatingInput.tsx
+++ Frontend/components/UI/Form/FloatingInput.tsx
@@ -2,26 +2,33 @@ import React from "react";
 import { Form } from "react-bootstrap";
 
 interface PropsType {
-  type: string;
-  placeholder: string;
-  className: string;
-  title: string;
+  Type: string;
+  Title: string;
+  Required?: boolean;
+  LabelClassName?: string;
+  InputClassName?: string;
+  ValidationMessage?: string;
 }
 
 const FloatingInput = React.forwardRef<HTMLInputElement, PropsType>((props, ref) => {
   return (
     <div className="row">
-      <Form.Floating>
-        <Form.Control
-          type={props.type}
-          placeholder={props.placeholder}
-          className=" border border-1 shadow-sm rounded-3 floatinginput "
-          ref={ref}
-        />
-        <label htmlFor="floatingInputCustom" className={props.className}>
-          <h6 className="px-1"> {props.title}</h6>
-        </label>
-      </Form.Floating>
+      <Form.Group controlId="validationCustom01">
+        <Form.Floating>
+          <Form.Control
+            Required={props.Required}
+            Type={props.Type}
+            className={"border border-1 shadow-sm rounded-4 " + props.InputClassName}
+            ref={ref}
+          />
+          <Form.Control.Feedback tooltip type="invalid">
+            {props.ValidationMessage}
+          </Form.Control.Feedback>
+          <label htmlFor="floatingInputCustom" className={props.LabelClassName}>
+            <h6> {props.Title}</h6>
+          </label>
+        </Form.Floating>
+      </Form.Group>
     </div>
   );
 });
